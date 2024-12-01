61dfdd30d9ce5a8c0f5f78bd0a2f276fa5cf9cac
Jack <jacktotten@hotmail.co.uk>
2024-12-01 22:49:33 +0000

    Adding MSE loss code

--- mulit_node_nn_example.py
+++ mulit_node_nn_example.py
@@ -4,6 +4,13 @@ import numpy as np
 def sigmoid(x: float):
     return 1/(np.exp(-x)+1)
 
+def mse_loss(y_true, y_pred):
+    return ((y_true - y_pred)**2).mean()
+
+def deriv_sigmoid(x):
+  fx = sigmoid(x)
+  return fx * (1 - fx)
+
 class Neuron:
     def __init__(self, weights, bias) -> None:
         self.weights = weights
